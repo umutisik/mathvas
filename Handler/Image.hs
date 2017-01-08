4ed460885b0525c985d7fab69c9c008ac4938e29
umutisik <umutisik@gmail.com>
2017-01-07 22:38:30 -0800

    pre-s

--- Handler/Image.hs
+++ Handler/Image.hs
@@ -4,19 +4,8 @@ import Import
 import Network.Wai (lazyRequestBody)
 import Data.FileEmbed (embedFile)
 
---temp
 import Settings.Environment
 
---import Data.Aeson (decode, Object)
---import Data.Aeson.Types (parseMaybe)
---import Data.Time.Clock.POSIX
-
---import System.Process
-
---getImageR :: Text -> Handler TypedContent
---getImageR name = liftIO $ do localBuildingPath' <- localBuildingPath
---                             file <- (readFile (unpack (localBuildingPath' ++ "outputimages/" ++ name)))::(IO ByteString)
---                             return ((TypedContent "image/jpeg" . toContent) file)
 
 getImageR :: ImageId -> Handler TypedContent
 getImageR imId =          do  mUserId <- maybeAuthId
@@ -35,3 +24,5 @@ getImageR imId =          do  mUserId <- maybeAuthId
 hasOwnership mUserId owner = case mUserId of
 	                             Just uid -> (owner == uid)
 	                             Nothing  -> False
+
+                               
