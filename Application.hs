f639fa932aa51139a247119482753f7b2e3d874d
umutisik <umutisik@gmail.com>
2017-01-08 17:43:05 -0800

    https wellknown serve

--- Application.hs
+++ Application.hs
@@ -48,6 +48,7 @@ import Handler.Lesson
 import Handler.Problem
 import Handler.Print
 import Handler.LessonImage
+import Handler.WellKnown
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ test/Handler/WellKnownSpec.hs
@@ -0,0 +1,10 @@
+module Handler.WellKnownSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getWellKnownR" $ do
+        error "Spec not implemented: getWellKnownR"
+
